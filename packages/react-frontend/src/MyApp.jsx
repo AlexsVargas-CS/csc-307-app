d3d4ff8ab16a9c3dc69199e3ab21ca146cf10009
Alexsvargas-CS <alexsvargascs@gmail.com>
2026-01-08 15:59:48 -0800

    added a Form component & linked to MyApp

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,6 +1,8 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
+
 
 const characters = [
   {
@@ -22,24 +24,7 @@ const characters = [
 ];
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    } ,
-    {
-    name: "Mac",
-    job: "Bouncer"
-    },
-    {
-    name: "Dee",
-    job: "Aspring actress"
-    },
-    {
-    name: "Dennis",
-    job: "Bartender"
-    } 
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -47,15 +32,17 @@ function MyApp() {
         });
         setCharacters(updated);
     }
+    
     return (
         <div className="container">
-        <Table 
-        characterData={characters} 
-        removeCharacter={removeOneCharacter}
-
+        <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
         />
+        <Form />
         </div>
     );
+    
 
     }
 
