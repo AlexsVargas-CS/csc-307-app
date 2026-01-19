b027b042bc2509a220928021c09100e4c35d1a83
Alexsvargas-CS <alexsvargascs@gmail.com>
2026-01-19 13:22:52 -0800

    fixed: errors

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,28 +5,6 @@ const port = 8000;
 
 app.use(express.json());
 
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-
-app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
-});
-
-app.listen(port, () => {
-
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
-
-});
-
 const users = {
   users_list: [
     {
@@ -56,3 +34,49 @@ const users = {
     }
   ]
 };
+
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
