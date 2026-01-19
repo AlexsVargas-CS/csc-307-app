2987fe32aef3cca9f75e4f60770c2fb013ebd145
Alexsvargas-CS <alexsvargascs@gmail.com>
2026-01-19 12:30:00 -0800

    updated

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,22 +5,18 @@ const port = 8000;
 
 app.use(express.json());
 
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
+const findUserByName = (id) => {
+  users["users_list"].find((users) => user["id" === id]);
 };
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
   } else {
-    res.send(users);
+    res.send(result);
   }
-
 });
 
 app.listen(port, () => {
