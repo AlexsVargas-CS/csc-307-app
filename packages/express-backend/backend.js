92fa3dcc2ae4505d6987cb2fa503e22e153c60b3
Alexsvargas-CS <alexsvargascs@gmail.com>
2026-01-19 11:23:57 -0800

    added: defined /users and displayed

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,9 +5,9 @@ const port = 8000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/users", (req, res) => {
 
-  res.send("Hello World!");
+  res.send(users);
 
 });
 
@@ -16,5 +16,35 @@ app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
-  
+
 });
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
